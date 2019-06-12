021076caf45700d234b0624daff506e2028c3ccf
WFD <752387064@qq.com>
2019-06-12 14:41:58 +0800

    数据更新

--- game-server/app/dao/characterDao.js
+++ game-server/app/dao/characterDao.js
@@ -1,21 +1,32 @@
 var roleLevel = require("../../config/role/roleLevel.json")
+var attributeType =   {
+	"characterId" : "number",
+	"name" : "string",
+    "level": "number",
+    "hp": "number",
+    "atk": "number",
+    "def": "number",
+    "atkSpeed": "number",
+    "crit": "number",
+    "critDef": "number",
+    "hitRate": "number",
+    "dodgeRate": "number",
+    "exp": "number"
+}
 var characterDao = function() {}
 //创建新角色
 characterDao.prototype.createCharacter = function(otps) {
 	var characterInfo = {
 		characterId : otps.characterId,
 		name : otps.name,
-		level : 1
+		level : 1,
+		exp : 0
 	}
-	for(var i in roleLevel[1]){
-		characterInfo[i] = roleLevel[1][i]
-	}
-	characterInfo.exp = 0
 	this.redisDao.db.hmset("area:area"+otps.areaId+":player:"+otps.uid+":characters:"+otps.characterId,characterInfo)
 	this.redisDao.db.hset("area:area"+otps.areaId+":player:"+otps.uid+":characterMap",otps.characterId,true)
 	return characterInfo
 }
-//获取英雄信息
+//获取角色信息
 characterDao.prototype.getCharacters = function(otps,cb) {
 	var self = this
 	self.redisDao.db.hgetall("area:area"+otps.areaId+":player:"+otps.uid+":characterMap",function(err,data) {
@@ -30,9 +41,25 @@ characterDao.prototype.getCharacters = function(otps,cb) {
 			}
 		}
 		self.redisDao.multi(multiList,function(err,list) {
+			for(var i = 0;i < list.length;i++){
+				list[i] = self.getCharacterAttribute(list[i])
+			}
 			cb(list)
 		})
 	})
+}
+//获取角色属性
+characterDao.prototype.getCharacterAttribute = function(character) {
+	for(var attribute in character){
+		if(attributeType[attribute] == "number"){
+			character[attribute] = Number(character[attribute])
+		}
+	}
+	var level = character["level"]
+    for(var attribute in roleLevel[level]){
+        character[attribute] = roleLevel[level][attribute]
+    }
+    return character
 }
 module.exports = {
 	id : "characterDao",
--- game-server/util/filters/areaFilter.js
+++ game-server/util/filters/areaFilter.js
@@ -5,7 +5,6 @@ module.exports = function() {
 var Filter = function() {}
 
 Filter.prototype.before = function (msg, session, next) {
-	console.log("判断是否已登录")
 	if(!session.get("uid")){
 		next("未登录")
 	}else{
