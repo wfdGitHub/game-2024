4d16bdc2b95f5ad8545e50f5dbdc0e1df4a57f3c
WFD <752387064@qq.com>
2019-06-21 14:20:10 +0800

    更新

--- game-server/app/domian/fight/seeded.js
+++ game-server/app/domian/fight/seeded.js
@@ -3,6 +3,7 @@ var seeded = function(seededNum) {
     this.random = function() {
         this.seededNum = (this.seededNum * 9301 + 49297) % 233280
         var rnd = this.seededNum / 233280
+        console.log("seeded : ",rnd)
         return rnd
     }
 }
