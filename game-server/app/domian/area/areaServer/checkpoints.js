5d42a0ff90e601821302c3dc2d420d3c69aec424
WFD <752387064@qq.com>
2019-06-27 11:35:31 +0800

    更新

--- game-server/app/domian/area/areaServer/checkpoints.js
+++ game-server/app/domian/area/areaServer/checkpoints.js
@@ -85,8 +85,9 @@ module.exports = function() {
 			var tmpTime = Math.floor((curTime - onhookLastTime) / (60 * 1000))
 			// console.log("tmpTime ",tmpTime)
 			if(tmpTime < 5){
-				cb(false,"time is too short "+tmpTime)
-			  	return
+				tmpTime = 5
+				// cb(false,"time is too short "+tmpTime)
+			 //  	return
 			}
 			self.getCheckpointsInfo(uid,function(level) {
 				if(!checkpointsCfg[level]){
