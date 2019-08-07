876ca08f159ce899c01290791f573a8d3c9a4e82
WFD <752387064@qq.com>
2019-08-07 18:39:02 +0800

    伙伴技能

--- game-server/app/domian/entity/partner.js
+++ game-server/app/domian/entity/partner.js
@@ -1,7 +1,14 @@
 var characterFun = require("./character.js")
+var advanceCfg = require("../../../config/gameCfg/advance.json")
 //伙伴
 var partner = function(otps) {
     console.log("new partner")
+    if(otps.advance){
+    	if(advanceCfg[otps.advance] && advanceCfg[otps.advance]["partner_"+otps.characterId]){
+    		otps.skills = advanceCfg[otps.advance]["partner_"+otps.characterId]
+    		console.log("otps.skills",otps.skills)
+    	}
+    }
     characterFun.call(this,otps)
 }
 partner.prototype = characterFun.prototype
