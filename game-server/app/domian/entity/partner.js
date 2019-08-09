c6c071615587e253c61ca146a5b519083c825786
WFD <752387064@qq.com>
2019-08-09 11:14:19 +0800

    转生被动技能，实际战斗生效

--- game-server/app/domian/entity/partner.js
+++ game-server/app/domian/entity/partner.js
@@ -9,6 +9,18 @@ var partner = function(otps) {
     		console.log("otps.skills",otps.skills)
     	}
     }
+    //转生被动技能列表
+    var passives = []
+    if(otps.passives){
+    	passives = JSON.parse(otps.passives) || []
+    }
+    var samsara = Math.floor(((otps.level - 1) / 100))
+    for(var i = 1;i <= samsara;i++){
+    	if(otps["ps_"+i]){
+    		passives.push(otps["ps_"+i])
+    	}
+    }
+    otps.passives = JSON.stringify(passives)
     characterFun.call(this,otps)
 }
 partner.prototype = characterFun.prototype
