f270fdd7f31a9fad648605c83dea8608e6210c27
WFD <752387064@qq.com>
2019-08-12 11:38:06 +0800

    报错日志追踪

---
+++ game-server/app/servers/connector/handler/logHandler.js
@@ -0,0 +1,37 @@
+var bearcat = require("bearcat")
+//日志模块
+var logHandler = function(app) {
+  this.app = app;
+	this.areaManager = this.app.get("areaManager")
+};
+//接收日志
+logHandler.prototype.addLogs = function(msg, session, next) {
+  var uid = session.get("uid")
+  var areaId = session.get("areaId")
+  var info = {
+    uid : uid,
+    areaId : areaId,
+    message : msg.message,
+    source : msg.source,
+    lineno : msg.lineno,
+    colno : msg.colno,
+    stack : msg.stack
+  }
+  this.redisDao.db.rpush("client:logs",JSON.stringify(info))
+  next(null)
+}
+
+module.exports = function(app) {
+  return bearcat.getBean({
+  	id : "logHandler",
+  	func : logHandler,
+  	args : [{
+  		name : "app",
+  		value : app
+  	}],
+    props : [{
+      name : "redisDao",
+      ref : "redisDao"
+    }]
+  })
+};
