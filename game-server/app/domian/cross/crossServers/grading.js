f792fcaf1363a9b2fc1b8b257c2d039007a96409
WFD <752387064@qq.com>
2020-03-25 16:14:41 +0800

    更新

--- game-server/app/domian/cross/crossServers/grading.js
+++ game-server/app/domian/cross/crossServers/grading.js
@@ -18,7 +18,7 @@ module.exports = function() {
 	this.gradingDayUpdate = function() {
 		self.redisDao.db.del("cross:grading:count")
 		self.redisDao.db.hget("cross:grading","month",function(err,data) {
-			if(data != util.getMonth())
+			// if(data != util.getMonth())
 				self.newGrading()
 		})
 	}
@@ -27,6 +27,7 @@ module.exports = function() {
 		console.log("新赛季开始")
 		self.redisDao.db.hset("cross:grading","month",util.getMonth())
 		self.redisDao.db.del("cross:grading:rank")
+		self.redisDao.db.del("cross:grading:award")
 		self.redisDao.db.get("area:lastid",function(err,lastid) {
 			var rankList = ["cross:grading:rank"]
 			for(var i in grading_robot){
@@ -40,6 +41,45 @@ module.exports = function() {
 		let uid = self.players[crossUid]["uid"]
 		let sid = self.players[crossUid]["areaId"]
 		let key = sid+"."+uid
+		let info = {
+			score : 0,
+			grading_award_list : [],
+			count : 0,
+			recordList : []
+		}
+		async.waterfall([
+			function(next) {
+				self.redisDao.db.zscore(["cross:grading:rank",key],function(err,score) {
+					if(score)
+						info.score = Number(score)
+					next()
+				})
+			},
+			function(next) {
+				self.redisDao.db.hget("cross:grading:count",key,function(err,count) {
+					if(count)
+						info.count = Number(count)
+					next()
+				})
+			},
+			function(next) {
+				let list = []
+				for(let glv in grading_lv)
+					list.push(key+"_"+glv)
+				self.redisDao.db.hmget("cross:grading:award",list,function(err,data) {
+					info.grading_award_list = data
+					next()
+				})
+			},
+			function(next) {
+				self.redisDao.db.lrange("cross:grading:record:"+key,0,-1,function(err,list) {
+					info.recordList = list
+					cb(true,info)
+				})
+			}
+		],function(err) {
+			cb(false,err)
+		})
 	}
 	//使用挑战券
 	this.useGradingItem = function(crossUid,cb) {
@@ -48,7 +88,7 @@ module.exports = function() {
 		let key = sid+"."+uid
 		this.consumeItems(crossUid,grading_cfg["item"]["value"]+":"+1,1,function(flag,err) {
 			if(flag){
-				self.redisDao.db.hincrby("cross:grading:count",key,1,function(err,value) {
+				self.redisDao.db.hincrby("cross:grading:count",key,-1,function(err,value) {
 					cb(true,value)
 				})
 			}else{
@@ -61,17 +101,18 @@ module.exports = function() {
 		let uid = self.players[crossUid]["uid"]
 		let sid = self.players[crossUid]["areaId"]
 		let key = sid+"."+uid
-		let info = {}
 		let glv,targetSid,targetUid,targetScore,targetInfo,atkTeam,defTeam,curScore,change,seededNum,winFlag
 		async.waterfall([
 			function(next) {
 				self.redisDao.db.hget("cross:grading:count",key,function(err,count) {
 					if(count && count >= grading_cfg["free_count"]["value"])
 						next("次数不足")
-					else
+					else{
+						self.redisDao.db.hincrby("cross:grading:count",key,1)
 						next()
+					}
 				})
-			}
+			},
 			function(next) {
 				self.redisDao.db.zrevrank(["cross:grading:rank",key],function(err,rank) {
 					if(rank === null){
@@ -80,7 +121,6 @@ module.exports = function() {
 					let begin = rank > 5 ? rank - 5 : 0
 					let end = rank + 5
 					self.redisDao.db.zrevrange(["cross:grading:rank",begin,end,"WITHSCORES"],function(err,list) {
-						console.log(list)
 						for(let i = 0;i < list.length;i++){
 							if(list[i] == key){
 								list.splice(i,2)
@@ -116,12 +156,11 @@ module.exports = function() {
 				})
 			},
 			function(next) {
-				console.log("对阵双方",atkTeam,defTeam)
 				seededNum = Date.now()
 				winFlag = self.fightContorl.beginFight(atkTeam,defTeam,{seededNum : seededNum})
-				if(winFlag){
+				if(winFlag || true){
 					change = grading_cfg["win_score"]["value"]
-					self.redisDao.db.zincrby(["cross:grading:rank",grading_cfg["win_score"]["value"]],function(err,value) {
+					self.redisDao.db.zincrby(["cross:grading:rank",grading_cfg["win_score"]["value"],key],function(err,value) {
 						curScore = value
 						next()
 					})
@@ -139,7 +178,6 @@ module.exports = function() {
 			},
 			function(next) {
 				glv = util.binarySearchIndex(grading_lv_list,curScore)
-				console.log("glv",glv,curScore)
 				let awardList = self.addItemStr(crossUid,grading_lv[glv]["challenge_award"],1)
 				let info = {
 					winFlag : winFlag,
@@ -151,8 +189,14 @@ module.exports = function() {
 					targetScore : targetScore,
 					targetInfo : targetInfo,
 					curScore : curScore,
+					time : Date.now(),
 					change : change
 				}
+				self.redisDao.db.rpush("cross:grading:record:"+key,JSON.stringify(info),function(err,num) {
+					if(num > 10){
+						self.redisDao.db.ltrim("cross:grading:record:"+key,-10,-1)
+					}
+				})
 				cb(true,info)
 			}
 		],function(err) {
@@ -161,6 +205,21 @@ module.exports = function() {
 	}
 	//领取段位奖励
 	this.gainGradingAward = function(crossUid,glv,cb) {
-		
+		let uid = self.players[crossUid]["uid"]
+		let sid = self.players[crossUid]["areaId"]
+		let key = sid+"."+uid
+		if(!grading_lv[glv] || !grading_lv[glv]["grading_award"]){
+			cb(false,"段位不存在")
+			return
+		}
+		self.redisDao.db.hget("cross:grading:award",key+"_"+glv,function(err,data) {
+			if(data){
+				cb(false,"已领取")
+				return
+			}
+			self.redisDao.db.hset("cross:grading:award",key+"_"+glv,1)
+			let awardList = self.addItemStr(crossUid,grading_lv[glv]["grading_award"],1)
+			cb(true,awardList)
+		})
 	}
 }
--- game-server/app/servers/cross/handler/gradingHandler.js
+++ game-server/app/servers/cross/handler/gradingHandler.js
@@ -11,7 +11,28 @@ gradingHandler.prototype.matchGrading = function(msg, session, next) {
     next(null,{flag : flag,data : data})
   })
 }
-
+//获取跨服段位赛数据
+gradingHandler.prototype.getGradingData = function(msg, session, next) {
+  var crossUid = session.get("crossUid")
+  this.crossManager.getGradingData(crossUid,function(flag,data) {
+    next(null,{flag : flag,data : data})
+  })
+}
+//使用挑战券
+gradingHandler.prototype.useGradingItem = function(msg, session, next) {
+  var crossUid = session.get("crossUid")
+  this.crossManager.useGradingItem(crossUid,function(flag,data) {
+    next(null,{flag : flag,data : data})
+  })
+}
+//领取段位奖励
+gradingHandler.prototype.gainGradingAward = function(msg, session, next) {
+  var crossUid = session.get("crossUid")
+  var glv = msg.glv
+  this.crossManager.gainGradingAward(crossUid,glv,function(flag,data) {
+    next(null,{flag : flag,data : data})
+  })
+}
 module.exports = function(app) {
   return bearcat.getBean({
   	id : "gradingHandler",
