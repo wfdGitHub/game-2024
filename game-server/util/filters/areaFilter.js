24df43b991bef3fbe54444787660ca42b9452f34
WFD <752387064@qq.com>
2020-04-03 13:33:44 +0800

    更新

--- game-server/util/filters/areaFilter.js
+++ game-server/util/filters/areaFilter.js
@@ -5,7 +5,7 @@ module.exports = function() {
 var Filter = function() {}
 
 Filter.prototype.before = function (msg, session, next) {
-	if(!session.uid || !session.get("areaId") || !!session.get("oriId")){
+	if(!session.uid || !session.get("areaId") || !session.get("oriId")){
 		next("未登录服务器")
 	}else{
 		next();
