68579baf5a4f4b2c43fae5f9ce3290dd34ac9db5
WFD <752387064@qq.com>
2020-06-05 19:03:05 +0800

    更新

--- game-server/app/servers/area/handler/normalHandler.js
+++ game-server/app/servers/area/handler/normalHandler.js
@@ -99,30 +99,33 @@ normalHandler.prototype.changeHead = function(msg, session, next) {
 }
 //增加物品  测试功能
 normalHandler.prototype.addItem = function(msg, session, next) {
-  var uid = session.uid
-  var areaId = session.get("areaId")
-  var itemId = msg.itemId
-  var value = msg.value
-  this.areaManager.areaMap[areaId].addItem({uid : uid,itemId : itemId,value : value},function(flag,data) {
-    next(null,{flag : flag,data : data})
-  })
+  // var uid = session.uid
+  // var areaId = session.get("areaId")
+  // var itemId = msg.itemId
+  // var value = msg.value
+  // this.areaManager.areaMap[areaId].addItem({uid : uid,itemId : itemId,value : value},function(flag,data) {
+  //   next(null,{flag : flag,data : data})
+  // })
+  next(null,{flag : false})
 }
 //增加物品str  测试功能
 normalHandler.prototype.addItemStr = function(msg, session, next) {
-  var uid = session.uid
-  var areaId = session.get("areaId")
-  var str = msg.str
-  var rate = msg.rate
-  var awardList = this.areaManager.areaMap[areaId].addItemStr(uid,str,rate)
-  next(null,{flag : true,awardList : awardList})
+  // var uid = session.uid
+  // var areaId = session.get("areaId")
+  // var str = msg.str
+  // var rate = msg.rate
+  // var awardList = this.areaManager.areaMap[areaId].addItemStr(uid,str,rate)
+  // next(null,{flag : true,awardList : awardList})
+  next(null,{flag : false})
 }
 //开启宝箱  测试功能
 normalHandler.prototype.openChestStr = function(msg, session, next) {
-  var uid = session.uid
-  var areaId = session.get("areaId")
-  var str = msg.str
-  var str = this.areaManager.areaMap[areaId].openChestStr(uid,str)
-  next(null,{flag : true,str : str})
+  // var uid = session.uid
+  // var areaId = session.get("areaId")
+  // var str = msg.str
+  // var str = this.areaManager.areaMap[areaId].openChestStr(uid,str)
+  // next(null,{flag : true,str : str})
+  next(null,{flag : false})
 }
 //获取商城数据
 normalHandler.prototype.getShopData = function(msg, session, next) {
