42b95c5ab9609b679b43c5e7801602f6638761d6
WFD <752387064@qq.com>
2020-06-05 15:04:14 +0800

    更新

--- game-server/app/dao/playerDao.js
+++ game-server/app/dao/playerDao.js
@@ -1,6 +1,6 @@
 var bearcat = require("bearcat")
 var playerDao = function() {}
-var beginHero = 104010
+var beginHero = 204010
 //创建角色
 playerDao.prototype.createPlayer = function(otps,cb) {
 	var playerInfo = {
