ae44ae8bb795e1a12dcf5f3e2251e032e71c9241
WFD <752387064@qq.com>
2020-06-22 09:50:44 +0800

    更新

--- game-server/app/domian/area/area.js
+++ game-server/app/domian/area/area.js
@@ -299,6 +299,7 @@ area.prototype.getPlayerInfoByUids = function(uids,cb) {
 			let info = {}
 			if(uids[i] < 10000){
 				info = self.robots[uids[i]]
+				info.head = 504010
 			}else{
 				info = {
 					uid : uids[i],
