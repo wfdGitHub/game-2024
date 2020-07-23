88758d5a19d9d7f6b6cd32f3c8b0c1b9f0576da7
WFD <752387064@qq.com>
2020-07-23 13:29:14 +0800

    更新

--- game-server/app/domian/admin/serverManager.js
+++ game-server/app/domian/admin/serverManager.js
@@ -41,7 +41,7 @@ serverManager.prototype.init = function() {
 		var xmlStr = local.decode(data.nt_data,Callback_Key)
 		console.log(xmlStr)
 		parseString(xmlStr,function(err,info) {
-			console.log(info)
+			console.log(info.quicksdk_message,info.quicksdk_message.message)
 		});
         res.send("FAILD")
 	})
