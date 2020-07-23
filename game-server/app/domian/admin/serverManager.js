4dca9c5b89d73eb81d9aad5c9c2972cb5f45167a
WFD <752387064@qq.com>
2020-07-23 18:15:14 +0800

    更新

--- game-server/app/domian/admin/serverManager.js
+++ game-server/app/domian/admin/serverManager.js
@@ -34,7 +34,7 @@ serverManager.prototype.init = function() {
 		var data = req.body
 		self.pay_order(data,function(flag,err) {
 			if(!flag)
-				res.send(err)
+				res.send("SUCCESS")
 			else
 				res.send("SUCCESS")
 		})
