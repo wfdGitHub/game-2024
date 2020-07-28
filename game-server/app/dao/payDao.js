34817285cb2b6fdc2d87c67ddc754c564c8e36c5
WFD <752387064@qq.com>
2020-07-28 10:08:05 +0800

    更新

--- game-server/app/dao/payDao.js
+++ game-server/app/dao/payDao.js
@@ -37,37 +37,37 @@ payDao.prototype.finishGameOrder = function(otps,cb) {
 	console.log("finishGameOrder",otps)
 	var self = this
 	var sql = "select * from game_order where game_order = ?"
-	self.db.query(sql,[otps.otps], function(err, res) {
-		try{
-			res =JSON.parse( JSON.stringify(res))
-			var data = res[0]
-			if(err || !data){
-				console.error("订单不存在",err)
-				self.faildOrder("订单不存在",otps)
-				cb(false,"finishGameOrder game_order err")
+	self.db.query(sql,[otps.game_order], function(err, res) {
+		if(err || !res){
+			self.faildOrder("订单不存在",otps)
+			cb(false,"finishGameOrder game_order err")
+			return
+		}
+		res =JSON.parse( JSON.stringify(res))
+		var data = res[0]
+		if(err || !data){
+			console.error("订单不存在",err)
+			self.faildOrder("订单不存在",otps)
+			cb(false,"finishGameOrder game_order err")
+		}else{
+			if(data.status == 0){
+				self.faildOrder("订单已完成",otps,data)
+				cb(false,null,data)
+			}else if(Number(otps.amount) != data.amount){
+				self.faildOrder("充值金额不对应",otps,data)
+				cb(false,"充值金额不对应",data)
 			}else{
-				if(data.status == 0){
-					self.faildOrder("订单已完成",otps,data)
-					cb(false,null,data)
-				}else if(Number(otps.amount) != data.amount){
-					self.faildOrder("充值金额不对应",otps,data)
-					cb(false,"充值金额不对应",data)
+				if(otps.status != 0){
+					//支付失败
+					sql = 'update game_order SET status=? where game_order = ?'
+					self.db.query(sql,[otps.status,otps.game_order],function(){})
+					cb(false,"充值失败")
 				}else{
-					if(otps.status != 0){
-						//支付失败
-						sql = 'update game_order SET status=? where game_order = ?'
-						self.db.query(sql,[otps.status,otps.game_order],function(){})
-						cb(false,"充值失败")
-					}else{
-						sql = 'update game_order SET pay_time=?,status=0,order_no=?,channel_code=?,channel_uid=? where game_order = ?'
-						self.db.query(sql,[Date.now(),otps.order_no,otps.channel,otps.channel_uid,otps.game_order],function(){})
-						cb(true,null,data)
-					}
+					sql = 'update game_order SET pay_time=?,status=0,order_no=?,channel_code=?,channel_uid=? where game_order = ?'
+					self.db.query(sql,[Date.now(),otps.order_no,otps.channel,otps.channel_uid,otps.game_order],function(){})
+					cb(true,null,data)
 				}
 			}
-		}catch(err){
-			console.err(res)
-			self.faildOrder("订单处理错误",otps)
 		}
 	})
 }
@@ -92,18 +92,20 @@ module.exports = {
 
 // var model = new payDao()
 // model.init()
-// // model.createGameOrder({
-// // 		pay_id : 5004,
-// // 		userName : "wfd",
-// // 		unionid : 123,
-// // 		accId : 1234,
-// // 		uid : 12345,
-// // 		channel_code : 12,
-// // 		channel_uid : 12,
-// // 		areaId : 1
-// // 	},function(flag,data) {
-// // 	console.log(flag,data)
-// // })
-// model.finishGameOrder({game_order:"39cd4e80-cbef-11ea-a61f-ab65f912efbd",order_no:"sdasdasa",status:0,amount:"12.8",channel_uid : 12,channel : "12"},(flag,str,data)=>{
+// model.createGameOrder({
+// 		pay_id : 5004,
+// 		userName : "wfd",
+// 		unionid : 123,
+// 		accId : 1234,
+// 		uid : 12345,
+// 		channel_code : 12,
+// 		channel_uid : 12,
+// 		areaId : 1
+// 	},function(flag,data) {
+// 	console.log(flag,data)
+// })
+// model.finishGameOrder({game_order:"0092a790-d000-11ea-a2a4-1f542f74ca49",order_no:"sdasdasa",status:0,amount:"12.8",channel_uid : 12,channel : "12"},(flag,str,data)=>{
 // 	console.log(flag,str,data)
 // })
+	// var sql = "select * from game_order where game_order = ?"
+	// model.db.query(sql,["0092a790-d000-11ea-a2a4-1f542f74ca49"], function(err, res) {console.log(res)})
