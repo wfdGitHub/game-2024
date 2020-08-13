9599b567db43727ec8370592c1589adf9f1747d7
WFD <752387064@qq.com>
2020-08-13 14:33:58 +0800

    更新

--- game-server/app/dao/mysql/mysql.js
+++ game-server/app/dao/mysql/mysql.js
@@ -9,7 +9,7 @@ var NND = {};
  * Init sql connection pool 
  */  
 NND.init = function () {  
-    _pool = require('./dao-pool').createMysqlPool();  
+    _pool = require('./dao-pool').createMysqlPool();
 };  
   
 /** 
@@ -20,24 +20,27 @@ NND.init = function () {
  *  
  */  
 NND.query = function (sql, args, cb) {  
-    var promise = _pool.acquire();  
-    promise.then(function (client) {  
-        client.query(sql, args, function (error, results, fields) {  
-            if (error) {  
+    if(_pool.isBorrowedResource()){
+        console.error("存在未释放的连接")
+    }
+    const resourcePromise = _pool.acquire();
+    resourcePromise
+      .then(function(client) {
+        client.query(sql, args, function(error, results, fields) {
+            if(error){
                 _pool.destroy(client);  
                 cb(error, results);  
-            }  
-            else {  
+            }
+            else{  
                 _pool.release(client);  
                 cb(error, results);  
-            }  
-        });  
-    }, function () {  
-        console.log("reject");  
-    }).catch(function (err) {  
-        cb(err);  
-        console.log(err);  
-    });  
+            }
+        });
+      })
+      .catch(function(err) {
+        _pool.release(client);  
+        cb(error)
+      });
 };  
   
 /** 
