a59adca072b36d524616011849ec40219f0d9849
WFD <752387064@qq.com>
2020-08-21 19:18:03 +0800

    更新

--- game-server/app/domian/cross/crossServers/escort.js
+++ game-server/app/domian/cross/crossServers/escort.js
@@ -311,8 +311,7 @@ module.exports = function() {
 			cb(false,"正在押镖中")
 			return
 		}
-		if((this.curHours < escort_cfg["closeTime1"]["value"] && this.curHours >= (escort_cfg["closeTime1"]["value"] - 0.17))
-		 || (this.curHours < escort_cfg["closeTime2"]["value"] && this.curHours >= (escort_cfg["closeTime2"]["value"] - 0.17))){
+		if((this.curHours < escort_cfg["closeTime1"]["value"] && this.curHours >= (escort_cfg["closeTime1"]["value"] - 0.17))){
 			cb(false,"现在不能押镖")
 			return
 		}
