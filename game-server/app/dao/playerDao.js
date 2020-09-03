48c7db2a6083c2f358588389ded84cc1aefb3a79
WFD <752387064@qq.com>
2020-09-03 16:01:51 +0800

    更新

--- game-server/app/dao/playerDao.js
+++ game-server/app/dao/playerDao.js
@@ -31,6 +31,7 @@ playerDao.prototype.createPlayer = function(otps,cb) {
 	self.redisDao.db.incrby("user:lastid",1,function(err,uid) {
 		uid = parseInt(uid)
 		playerInfo.uid = uid
+		playerInfo.CE = 0
 		self.redisDao.db.hset("acc:user:"+playerInfo.accId+":playerMap",uid,otps.areaId)
 		self.redisDao.db.hset("acc:user:"+playerInfo.accId+":areaMap",otps.areaId,uid)
 		self.redisDao.db.hmset("player:user:"+uid+":playerInfo",playerInfo,function(err,data) {
