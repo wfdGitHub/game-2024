9a6a3573b844db2952114d3d60149004a64a840e
WFD <752387064@qq.com>
2020-09-10 09:32:16 +0800

    更新

--- game-server/app/dao/cacheDao.js
+++ game-server/app/dao/cacheDao.js
@@ -5,6 +5,9 @@ var RDS_HOST = daoConfig.cache.host
 var RDS_PWD = daoConfig.cache.pwd
 var RDS_OPTS = {auth_pass : RDS_PWD}
 var publish = "jianwan"
+var publish_normal = "message_"+publish+"_normal"
+var publish_chat = "message_"+publish+"_chat"
+var publish_item = "message_"+publish+"_item"
 var cacheDao = function() {
 	this.db = false
 }
@@ -20,15 +23,15 @@ cacheDao.prototype.init = function(cb) {
 }
 cacheDao.prototype.saveCache = function(info) {
 	info.time = Date.now()
-	this.db.rpush("message_"+publish,JSON.stringify(info))
+	this.db.rpush(publish_normal,JSON.stringify(info))
 }
 cacheDao.prototype.saveChat = function(info) {
 	info.time = Date.now()
-	this.db.rpush("message_"+publish+"_chat",JSON.stringify(info))
+	this.db.rpush(publish_chat,JSON.stringify(info))
 }
 cacheDao.prototype.saveItemChange = function(info) {
 	info.time = Date.now()
-	this.db.rpush("message_"+publish+"_item",JSON.stringify(info))
+	this.db.rpush(publish_item,JSON.stringify(info))
 }
 module.exports = {
 	id : "cacheDao",
