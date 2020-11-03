8a57b22c88da5c498e74ffdefed15f31d271146f
WFD <752387064@qq.com>
2020-11-03 10:06:56 +0800

    更新

--- game-server/app.js
+++ game-server/app.js
@@ -24,7 +24,7 @@ bearcat.start(function() {
       app.set('connectorConfig',
         {
           connector : pomelo.connectors.hybridconnector,
-          heartbeat : 30,
+          heartbeat : 60,
           disconnectOnTimeout : true,
           useDict : true,
           useProtobuf : true
