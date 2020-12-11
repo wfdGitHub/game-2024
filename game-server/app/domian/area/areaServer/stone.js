517e350b097545c0df4bb45d6173906561599247
WFD <752387064@qq.com>
2020-12-11 18:16:05 +0800

    更新

--- game-server/app/domian/area/areaServer/stone.js
+++ game-server/app/domian/area/areaServer/stone.js
@@ -254,10 +254,11 @@ module.exports = function() {
 				if(!flag){
 					cb(false,err)
 				}else{
-					if(self.checkLimitedTime("kuangchang"))
+					if(self.checkLimitedTime("kuangchang")){
 						var awardList = self.openChestStrNoItem("pickaxe_limited_"+pId)
-					else
+					}else{
 						var awardList = self.openChestStrNoItem("pickaxe_"+pId)
+					}
 					var info = {}
 					info["award_"+pos] = awardList[0]
 					info["time_"+pos] = Date.now() + default_cfg["pickaxe_time_"+pId]["value"]
