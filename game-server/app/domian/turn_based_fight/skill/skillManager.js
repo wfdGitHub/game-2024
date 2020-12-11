8c142335747803bb860c8c5d931e9da211bbc2b0
WFD <752387064@qq.com>
2020-12-11 17:03:12 +0800

    更新

--- game-server/app/domian/turn_based_fight/skill/skillManager.js
+++ game-server/app/domian/turn_based_fight/skill/skillManager.js
@@ -88,11 +88,11 @@ model.useSkill = function(skill,chase) {
 		//判断技能目标减少
 		if(skill.character.less_skill_buffRate){
 			var allLenth = this.locator.getTargetsNum(skill.targetType)
-			buffRate = buffRate * (1 + (allLenth - targets.length + 1) / allLenth) * skill.character.less_skill_buffRate
+			buffRate += buffRate * (1 + (allLenth - targets.length + 1) / allLenth) * skill.character.less_skill_buffRate
 		}
 		if(skill.character.less_buff_arg){
 			var allLenth = this.locator.getTargetsNum(skill.targetType)
-			buffArg = buffArg * (1 + ((allLenth - targets.length + 1) / allLenth)) * skill.character.less_buff_arg
+			buffArg += buffArg * (1 + ((allLenth - targets.length + 1) / allLenth)) * skill.character.less_buff_arg
 		}
 		// if(skill.buffId == "dizzy" && skill.character.dizzy_clear_anger)
 		// 	buffRate *= 0.5
