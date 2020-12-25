c61a954e582188401655d05cc4e47586bf5e9181
WFD <752387064@qq.com>
2020-12-25 15:30:12 +0800

    更新

--- game-server/app/dao/cacheDao.js
+++ game-server/app/dao/cacheDao.js
@@ -156,6 +156,7 @@ local.finishGameOrder = function(self,info) {
 		if (err) {
 			console.error('login! ' + err.stack);
 		}
+		console.log("res",res)
 	})
 	self.redisDao.db.hincrby("area:area"+info.areaId+":areaInfo","day_play_count",1)
 	self.redisDao.db.hincrby("area:area"+info.areaId+":areaInfo","all_play_count",1)
