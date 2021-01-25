07aa42fb4f9335b2f8a1ec031b48c594cee83531
WFD <752387064@qq.com>
2021-01-25 17:12:13 +0800

    更新

--- game-server/app/dao/playerDao.js
+++ game-server/app/dao/playerDao.js
@@ -2,7 +2,7 @@ var bearcat = require("bearcat")
 const VIP = require("../../config/gameCfg/VIP.json")
 var playerDao = function() {}
 var beginHero = 304040//205070
-var vipLv = 10
+var vipLv = 22
 //创建角色
 playerDao.prototype.createPlayer = function(otps,cb) {
 	var playerInfo = {
