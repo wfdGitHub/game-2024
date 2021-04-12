66cd5a2e96f4e7ddab09a4765802633adcae493c
WFD <752387064@qq.com>
2021-04-12 14:27:20 +0800

    更新

--- game-server/app/domian/area/areaServer/bag.js
+++ game-server/app/domian/area/areaServer/bag.js
@@ -34,7 +34,7 @@ module.exports = function() {
 				var heroId = itemCfg[otps.itemId].arg
 				var needValue = 30
 				self.heroDao.getHeroAmount(uid,function(flag,info) {
-				  	if(info.cur >= info.max){
+				  	if(info.cur + value > info.max){
 				    	next(null,{flag : false,data : "英雄背包已满"})
 				    	return
 				  	}
