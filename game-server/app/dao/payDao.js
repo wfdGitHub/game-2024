d84d02ee58ffaec8ea2b08b60b633305bf3c2584
WFD <752387064@qq.com>
2021-06-10 19:54:24 +0800

    更新

--- game-server/app/dao/payDao.js
+++ game-server/app/dao/payDao.js
@@ -41,7 +41,7 @@ payDao.prototype.finishGameOrder = function(otps,cb) {
 	var sql = "select * from game_order where game_order = ?"
 	var daySyr = (new Date()).toDateString()
 	self.db.query(sql,[otps.game_order], function(err, res) {
-		if(true || err || !res){
+		if(err || !res){
 			console.error(err)
 			self.faildOrder("订单不存在",otps)
 			cb(false,"finishGameOrder game_order err")
