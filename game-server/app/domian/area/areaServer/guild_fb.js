039e9c99e840dcc9ea67a964dfc3fae885055de5
WFD <752387064@qq.com>
2021-06-17 17:28:33 +0800

    更新

--- game-server/app/domian/area/areaServer/guild_fb.js
+++ game-server/app/domian/area/areaServer/guild_fb.js
@@ -138,7 +138,7 @@ module.exports = function() {
 							killFlag = true
 							self.sendMail(uid,"击杀同盟BOSS","恭喜您,成功击杀同盟BOSS。",currency+":"+guild_fb[fbId]["kill"])
 							self.addGuildEXP(guildId,guild_fb[fbId]["exp"])
-							self.addGuildGift(guildId,"洪荒异兽【"+(fbId+1)+"】",10,guild_fb[fbId]["gift"],oneDayTime)
+							self.addGuildGift(guildId,"同盟副本【"+(fbId+1)+"】",10,guild_fb[fbId]["gift"],oneDayTime)
 							self.redisDao.db.hincrby(main_name+":"+guildId,"curFB",1)
 							self.redisDao.db.zrange(main_name+":"+guildId+":"+fbId,-3,-1,function(err,list) {
 								var rank = 0
