0540d68209607acc06225b483ad706cb2135b9a8
WFD <752387064@qq.com>
2021-11-03 09:53:05 +0800

    更新

--- game-server/app/domian/turn_based_fight/buff/buffs/armor.js
+++ game-server/app/domian/turn_based_fight/buff/buffs/armor.js
@@ -2,7 +2,7 @@
 var buffBasic = require("../buffBasic.js")
 var model = function(releaser,character,otps) {
 	var buff = new buffBasic(releaser,character,otps)
-	buff.value = 0.08
+	buff.value = otps.buffArg
 	return buff
 }
 module.exports = model
