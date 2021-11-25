8351a8e602825745021b41b692e4b686356ec07f
WFD <752387064@qq.com>
2021-11-25 17:26:52 +0800

    更新

--- game-server/app/compose/namespace.js
+++ game-server/app/compose/namespace.js
@@ -1,17 +1,22 @@
-var FirstName = require("../../config/sysCfg/FirstName.json")
-var LastName = require("../../config/sysCfg/LastName.json")
+var FirstName1 = require("../../config/sysCfg/FirstName1.json")
+var FirstName2 = require("../../config/sysCfg/FirstName2.json")
+var LastName1 = require("../../config/sysCfg/LastName1.json")
+var LastName2 = require("../../config/sysCfg/LastName2.json")
 //命名组件
 var namespace = function() {
 	this.name = "namespace"
 }
 //获取随机名称
 namespace.prototype.getName = function() {
-	return FirstName[Math.floor(Math.random() * FirstName.length)] + " "+ LastName[Math.floor(Math.random() * LastName.length)]
+	if(Math.random() < 0.5)
+		return FirstName1[Math.floor(Math.random() * FirstName1.length)] + "•"+ LastName1[Math.floor(Math.random() * LastName1.length)]
+	else
+		return FirstName2[Math.floor(Math.random() * FirstName2.length)] + "•"+ LastName2[Math.floor(Math.random() * LastName2.length)]
 }
 //获取指定顺序名称
 namespace.prototype.getNameByIndex = function(id) {
 	var index = Math.floor(Math.sqrt(id))
-	return FirstName[index] + " "+ LastName[index]
+	return FirstName1[index] + "•"+ LastName1[index]
 }
 module.exports = {
 	id : "namespace",
