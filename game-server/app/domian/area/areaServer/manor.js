15f4df082aad3e90793bb7c35d0bd71ad1689c08
WFD <752387064@qq.com>
2022-03-23 15:21:22 +0800

    更新

--- game-server/app/domian/area/areaServer/manor.js
+++ game-server/app/domian/area/areaServer/manor.js
@@ -1,10 +1,12 @@
 //家园系统
 const manor_builds = require("../../../../config/gameCfg/manor_builds.json")
+const async = require("async")
 const builds = {}
 for(var i in manor_builds){
 	if(!builds[manor_builds[i]["basic"]])
 		builds[manor_builds[i]["basic"]] = require("../../../../config/gameCfg/manor_"+manor_builds[i]["basic"]+".json")
 }
+console.log(builds)
 const main_name = "manor"
 module.exports = function() {
 	var self = this
@@ -20,7 +22,7 @@ module.exports = function() {
 	}
 	//初始化家园
 	this.manor_init = function(uid,cb) {
-		self.setObj(uid,main_name,"main":1)
+		self.setObj(uid,main_name,"main",1)
 		cb(true,{"main":1})
 	}
 	//建设建筑
@@ -29,7 +31,25 @@ module.exports = function() {
 			cb(false,"bId error "+bId)
 			return
 		}
-		
+		async.waterfall([
+			function(next) {
+				//获取主建筑等级
+				self.getObj(uid,main_name,"main",function(mainLv) {
+					mainLv = Number(mainLv) || 0
+					if(manor_builds[bId]["main_lv"] > mainLv){
+						next("主建筑等级不足")
+						return
+					}
+					next()
+				})
+			},
+			function(next) {
+				// body...
+			}
+		],function(err) {
+			// body...
+			cb(false,err)
+		})
 	}
 	//拆除建筑
 	this.manor_clear = function(uid,bId,cb) {
