46a668eb1007e1f4f1dc811c9257f6da40e44a71
WFD <752387064@qq.com>
2022-05-11 13:52:47 +0800

    初始十星

--- game-server/app/dao/playerDao.js
+++ game-server/app/dao/playerDao.js
@@ -53,7 +53,7 @@ playerDao.prototype.createPlayer = function(otps,cb) {
 			if(!err){
 				self.redisDao.db.hset("game:nameMap",otps.name,uid)
 				if(!otps.robot){
-					self.heroDao.gainHero(otps.areaId,uid,{id : beginHero},function(flag,heroInfo) {
+					self.heroDao.gainHero(otps.areaId,uid,{id : beginHero,star:10},function(flag,heroInfo) {
 						self.heroDao.setFightTeam(otps.areaId,uid,[heroInfo.hId,0,null,null,null,null])
 					})
 					self.mysqlDao.addDaylyData("userNum",1)
