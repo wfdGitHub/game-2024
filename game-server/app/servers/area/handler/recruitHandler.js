3574a698125b6453ae55d83e1c3647bb2c52b4a9
WFD <752387064@qq.com>
2022-05-23 14:08:11 +0800

    招募 抽奖次数  升星任务

--- game-server/app/servers/area/handler/recruitHandler.js
+++ game-server/app/servers/area/handler/recruitHandler.js
@@ -85,35 +85,57 @@ recruitHandler.prototype.getTopicRecruitData = function(msg, session, next) {
     next(null,{flag : true,data : data})
   })
 }
+//道具招募
+recruitHandler.prototype.recruitHeroByItem = function(msg, session, next) {
+  var areaId = session.get("areaId")
+  var uid = session.uid
+  var type = msg.type
+  var count = msg.count
+  this.areaManager.areaMap[areaId].recruitHeroByItem(uid,type,count,function(flag,data) {
+    next(null,{flag : flag,data : data})
+  })
+}
+//元宝招募
+recruitHandler.prototype.recruitHeroByGold = function(msg, session, next) {
+  var areaId = session.get("areaId")
+  var uid = session.uid
+  var type = msg.type
+  var count = msg.count
+  this.areaManager.areaMap[areaId].recruitHeroByGold(uid,type,count,function(flag,data) {
+    next(null,{flag : flag,data : data})
+  })
+}
 //主题招募一次
 recruitHandler.prototype.topicRecruitOnce = function(msg, session, next) {
-  var uid = session.uid
-  var areaId = session.get("areaId")
-  var self = this
-  self.heroDao.getHeroAmount(uid,function(flag,info) {
-      if(info.cur + 1 > info.max){
-        next(null,{flag : false,data : "武将背包已满"})
-        return
-      }
-      self.areaManager.areaMap[areaId].topicRecruitOnce(uid,function(flag,heroInfos) {
-        next(null,{flag : true,heroInfos : heroInfos})
-      })
-  })
+  // var uid = session.uid
+  // var areaId = session.get("areaId")
+  // var self = this
+  // self.heroDao.getHeroAmount(uid,function(flag,info) {
+  //     if(info.cur + 1 > info.max){
+  //       next(null,{flag : false,data : "武将背包已满"})
+  //       return
+  //     }
+  //     self.areaManager.areaMap[areaId].topicRecruitOnce(uid,function(flag,heroInfos) {
+  //       next(null,{flag : true,heroInfos : heroInfos})
+  //     })
+  // })
+  next(null,{flag:false,err:"接口已弃用"})
 }
 //主题招募十次
 recruitHandler.prototype.topicRecruitMultiple = function(msg, session, next) {
-  var uid = session.uid
-  var areaId = session.get("areaId")
-  var self = this
-  self.heroDao.getHeroAmount(uid,function(flag,info) {
-      if(info.cur + 10 > info.max){
-        next(null,{flag : false,data : "武将背包已满"})
-        return
-      }
-      self.areaManager.areaMap[areaId].topicRecruitMultiple(uid,function(flag,heroInfos) {
-        next(null,{flag : true,heroInfos : heroInfos})
-      })
-  })
+  // var uid = session.uid
+  // var areaId = session.get("areaId")
+  // var self = this
+  // self.heroDao.getHeroAmount(uid,function(flag,info) {
+  //     if(info.cur + 10 > info.max){
+  //       next(null,{flag : false,data : "武将背包已满"})
+  //       return
+  //     }
+  //     self.areaManager.areaMap[areaId].topicRecruitMultiple(uid,function(flag,heroInfos) {
+  //       next(null,{flag : true,heroInfos : heroInfos})
+  //     })
+  // })
+  next(null,{flag:false,err:"接口已弃用"})
 }
 //领取主题招募奖励
 recruitHandler.prototype.gainTopicRecruitBoxAward = function(msg, session, next) {
