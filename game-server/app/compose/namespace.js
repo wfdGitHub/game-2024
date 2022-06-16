bce20493af3b7fa70811ece980a47e5dbe5a1eb3
wfd <752387064@qq.com>
2022-06-16 15:10:27 +0800

    更新

--- game-server/app/compose/namespace.js
+++ game-server/app/compose/namespace.js
@@ -6,6 +6,7 @@ var namespace = function() {
 //获取随机名称
 namespace.prototype.getName = function() {
 	return FirstName1[Math.floor(Math.random() * FirstName1.length)]
+}
 //获取指定顺序名称
 namespace.prototype.getNameByIndex = function(id) {
 	var index = Math.floor(Math.sqrt(id))
