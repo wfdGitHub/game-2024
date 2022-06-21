f4531b9744bfbba16d0ef4fa605146ed831f23e5
wfd <752387064@qq.com>
2022-06-21 10:36:19 +0800

    更新

--- game-server/app/domian/admin/serverDB.js
+++ game-server/app/domian/admin/serverDB.js
@@ -455,6 +455,8 @@ var model = function() {
 	//更新节日活动
 	posts["/setFestivalInfo"] = function(req,res) {
 		var data = req.body
+		console.log(typeof(data))
+		console.log(JSON.parse(data))
 		for(var i in data){
 			if(festivalBasicInfo[i] == "object" || festivalBasicInfo[i] == "array")
 				data[i] = JSON.parse(data[i])
