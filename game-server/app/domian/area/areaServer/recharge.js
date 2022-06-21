5ff5e544f43a6d1252ad07304c7d0a39a94b5f61
wfd <752387064@qq.com>
2022-06-21 16:09:03 +0800

    GM功能

--- game-server/app/domian/area/areaServer/recharge.js
+++ game-server/app/domian/area/areaServer/recharge.js
@@ -11,6 +11,7 @@ const gift_skin = require("../../../../config/gameCfg/gift_skin.json")
 const pay_cfg = require("../../../../config/gameCfg/pay_cfg.json")
 const gift_loop = require("../../../../config/gameCfg/gift_loop.json")
 const util = require("../../../../util/util.js")
+const GM_CFG = require("../../../../config/gameCfg/GM_CFG.json")
 const uuid = require("uuid")
 const main_name = "activity"
 const day31Time = 2592000000
@@ -41,12 +42,21 @@ module.exports = function() {
 			cb(false,"pay_id error")
 			return
 		}
-		self.consumeItems(uid,"110:"+pay_cfg[pay_id]["dianpiao"],1,"点票支付",function(flag,err) {
-			if(flag){
-				self.finish_recharge(uid,pay_id,cb)
-			}else{
-				cb(false,err)
+		var gmLv = self.getLordAtt(uid,"gmLv")
+		self.getPlayerData(uid,"diaopiao_use",function(value) {
+			value = Number(value) || 0
+			if((value + pay_cfg[pay_id]["dianpiao"]) > GM_CFG[gmLv]["dianpiao"]){
+				cb(false,"可用额度不足 "+value+"/"+GM_CFG[gmLv]["dianpiao"])
+				return
 			}
+			self.consumeItems(uid,"110:"+pay_cfg[pay_id]["dianpiao"],1,"点票支付",function(flag,err) {
+				if(flag){
+					self.incrbyPlayerData(uid,"diaopiao_use",pay_cfg[pay_id]["dianpiao"])
+					self.finish_recharge(uid,pay_id,cb)
+				}else{
+					cb(false,err)
+				}
+			})
 		})
 	}
 	//申请充值
@@ -124,6 +134,12 @@ module.exports = function() {
 			case "gift_skin":
 				this.buyLimitSkin(uid,pay_cfg[pay_id]["arg"],call_back.bind(this,uid))
 			break
+			case "wuxian":
+				this.buyWuxian(uid,pay_cfg[pay_id]["arg"],call_back.bind(this,uid))
+			break
+			case "gmLv":
+				this.buyGMLv(uid,pay_cfg[pay_id]["arg"],call_back.bind(this,uid))
+			break
 		}
 		var once_index = recharge_once_table[pay_id]
 		if(once_index){
@@ -168,6 +184,35 @@ module.exports = function() {
 			self.sendMail(uid,"充值奖励","感谢您的充值,这是您的充值奖励,请查收。",award)
 			cb(true)
 		})
+	}
+	//购买无限特权
+	this.buyWuxian = function(uid,id,cb) {
+		if(!wuxian[id] || !wuxian[id]["rmb"]){
+			cb(false,"无限特权不存在")
+			return
+		}
+		self.incrbyObj(uid,main_name,id,1)
+		var notify = {
+			type : "wuxian",
+			wuxianId : id
+		}
+		self.sendToUser(uid,notify)
+		cb(true)
+	}
+	//购买GM等级
+	this.buyGMLv = function(uid,id,cb) {
+		if(!GM_CFG[id] || !GM_CFG[id]["award"]){
+			cb(false,"GM特权不存在")
+			return
+		}
+		self.chageLordData(uid,"gmLv",id)
+		var notify = {
+			type : "gmLv",
+			lv : id
+		}
+		self.sendToUser(uid,notify)
+		self.sendMail(uid,"充值奖励","感谢您的充值,这是您的充值奖励,请查收。",GM_CFG[id]["award"])
+		cb(true)
 	}
 	//购买循环礼包
 	this.buyLoopGift = function(uid,loopId,cb) {
