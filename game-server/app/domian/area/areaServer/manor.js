c12c379b8a1e3380aaf6fc265e59c5cccbe85435
wfd <752387064@qq.com>
2022-08-23 09:59:07 +0800

    更新

--- game-server/app/domian/area/areaServer/manor.js
+++ game-server/app/domian/area/areaServer/manor.js
@@ -20,15 +20,19 @@ for(var i in manor_builds){
 }
 for(var i in builds["xmc"]){
 	builds["xmc"][i]["allWeight"] = 0
-	for(var j = 1; j <= 4;j++)
-		if(builds["xmc"][i]["quality_"+j])
-			builds["xmc"][i]["allWeight"] +=builds["xmc"][i]["quality_"+j]
+	for(var j = 1; j <= 4;j++){
+		if(!builds["xmc"][i]["quality_"+j])
+			builds["xmc"][i]["quality_"+j] = 0
+		builds["xmc"][i]["allWeight"] += builds["xmc"][i]["quality_"+j]
+	}
 }
 for(var i in builds["qjf"]){
 	builds["qjf"][i]["allWeight"] = 0
-	for(var j = 1; j <= 4;j++)
-		if(builds["qjf"][i]["quality_"+j])
-			builds["qjf"][i]["allWeight"] +=builds["qjf"][i]["quality_"+j]
+	for(var j = 1; j <= 4;j++){
+		if(!builds["qjf"][i]["quality_"+j])
+			builds["qjf"][i]["quality_"+j] = 0
+		builds["qjf"][i]["allWeight"] += builds["qjf"][i]["quality_"+j]
+	}
 }
 var mon_weight = {"all":10000,"1":3000,"2":5000,"3":7000,"4":8500,"5":9500,"6":10000}
 for(var i in builds["main"]){
