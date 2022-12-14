fbf2a1926e416d25b62ccb9faacf3728c4fd7a85
DESKTOP-EGH2IJ9\WFD <11>
2022-12-14 18:27:18 +0800

    服务器加固

--- game-server/util/filters/areaFilter.js
+++ game-server/util/filters/areaFilter.js
@@ -1,21 +1,12 @@
+const bearcat = require("bearcat")
 module.exports = function() {
   return new Filter();
 }
-
 var Filter = function() {}
-
 Filter.prototype.before = function (msg, session, next) {
 	if(!session.uid || !session.get("areaId") || !session.get("oriId")){
 		next("未登录服务器")
 	}else{
-		session.handlerTime = Date.now()
 		next();
 	}
-}
-Filter.prototype.after = function (err, msg, session, resp, next) {
-	var dt = Date.now() - session.handlerTime
-	if(dt > 200){
-		console.error("handlerTime over ",dt,msg)
-	}
-	next(err);
 }
