826a68c18e9db7c64fba75d9c2f9dbd97386153f
wfd <752387064@qq.com>
2023-01-03 11:09:31 +0800

    清档工具

--- web-server/serverDB.js
+++ web-server/serverDB.js
@@ -10,6 +10,8 @@ const heros = require("../game-server/config/gameCfg/heros.json")
 const lv_cfg = require("../game-server/config/gameCfg/lv_cfg.json")
 const star_base = require("../game-server/config/gameCfg/star_base.json")
 const stringRandom = require('string-random');
+const dataClean = require("./model/dataClean.js")
+const sqlClean = require("./model/sqlClean.js")
 var model = function() {
 	var self = this
 	var posts = {}
@@ -19,12 +21,24 @@ var model = function() {
 		self.mysqlDao = mysqlDao
 		self.redisDao = redisDao
 		self.server = server
+		dataClean.init(server,mysqlDao,redisDao)
+		sqlClean.init(server,mysqlDao,redisDao)
 		for(var key in posts){
 			server.post(key,posts[key])
 		}
         for(var i in item_cfg){
 			items[i] = item_cfg[i]["name"]
         }
+	}
+	//数据库清档
+	posts["/sqlClean"] = function(req,res) {
+		sqlClean.dataClean(function(flag,data) {
+			if(flag){
+				res.send("SUCCESS")
+			}else{
+				res.send(data)
+			}
+		})
 	}
 	//获取全服邮件
 	posts["/getAreaMailList"] = function(req,res) {
@@ -1134,6 +1148,16 @@ var model = function() {
 				}
 			}
 		})
+	}
+	//清理无效账号
+	posts["/cleanPlayer"] = function(req,res) {
+		dataClean.checkPlayer()
+		res.send({flag:true})
+	}
+	//清理数据库记录
+	posts["/cleanSql"] = function(req,res) {
+		dataClean.cleanMysqlLog()
+		res.send({flag:true})
 	}
 	local.getPlayerBaseByUids = function(uids,cb) {
 		if(!uids.length){
