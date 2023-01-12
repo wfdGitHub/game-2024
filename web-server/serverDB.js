9afec69c9a4cc3cee506719e7a6edac4285268e1
wfd <752387064@qq.com>
2023-01-12 17:26:39 +0800

    更新

--- web-server/serverDB.js
+++ web-server/serverDB.js
@@ -1,6 +1,7 @@
 //数据库查询
 const http = require("http")
 const uuid = require("uuid")
+const os = require('os');
 const querystring = require("querystring")
 const item_cfg = require("../game-server/config/gameCfg/item.json")
 const pay_cfg = require("../game-server/config/gameCfg/pay_cfg.json")
@@ -29,6 +30,14 @@ var model = function() {
         for(var i in item_cfg){
 			items[i] = item_cfg[i]["name"]
         }
+	}
+	//获取系统数据
+	posts["/getOSData"] = function(req,res) {
+		var info = {}
+		info.freemem = os.freemem()
+		info.totalmem = os.totalmem()
+		info.cpus = os.cpus()
+		res.send(info)
 	}
 	//数据库清档
 	posts["/sqlClean"] = function(req,res) {
