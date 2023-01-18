d6d6d47d22925fa09efad94de93a0c28ab4e3b86
wfd <752387064@qq.com>
2023-01-18 15:51:55 +0800

    更新

--- web-server/serverDB.js
+++ web-server/serverDB.js
@@ -4,6 +4,7 @@ const uuid = require("uuid")
 const querystring = require("querystring")
 const item_cfg = require("../game-server/config/gameCfg/item.json")
 const pay_cfg = require("../game-server/config/gameCfg/pay_cfg.json")
+const recharge_cfg = require("../game-server/config/gameCfg/recharge.json")
 const hufu_quality = require("../game-server/config/gameCfg/hufu_quality.json")
 const hufu_skill = require("../game-server/config/gameCfg/hufu_skill.json")
 const heros = require("../game-server/config/gameCfg/heros.json")
@@ -27,8 +28,14 @@ var model = function() {
 			server.post(key,posts[key])
 		}
         for(var i in item_cfg){
-			items[i] = item_cfg[i]["name"]+"("+item_cfg[i]["des"]+")"
+        	items[i] = item_cfg[i]["name"]+"("+item_cfg[i]["des"]+")"
+			// items[i] = item_cfg[i]["name"]
         }
+        items["item_cfg"] = item_cfg
+	}
+	//获取直充表
+	posts["/getRechargeCfg"] = function(req,res) {
+		res.send(recharge_cfg)
 	}
 	//获取系统数据
 	posts["/getOSData"] = function(req,res) {
@@ -114,7 +121,7 @@ var model = function() {
 	}
     //获取物品表
     posts["/get_items"] = function(req,res) {
-        res.send(items)
+        res.send(item_cfg)
     }
     //获取支付表
     posts["/get_pay_cfg"] = function(req,res) {
@@ -209,7 +216,9 @@ var model = function() {
 		var info = {
 			"title" : data.title,
 			"text" : data.text,
-			"rate" : data.rate
+			"rate" : data.rate,
+			"beginTime" : data.beginTime,
+			"endTime" : data.endTime
 		}
 		self.redisDao.db.hset("rebate_gold_map",data.id,JSON.stringify(info),function(err) {
 			var url = "http://127.0.0.1:5081/updateRebate"
@@ -230,6 +239,37 @@ var model = function() {
 	posts["/getAreaName"] = function(req,res) {
 		self.redisDao.db.hgetall("area:areaName",function(err,data) {
 			res.send({areaNames:data || {}})
+		})
+	}
+	//获取返利现金券
+	posts["/getRebateRMB"] = function(req,res) {
+		self.redisDao.db.hgetall("rebate_rmb_map",function(err,data) {
+			res.send(data)
+		})
+	}
+	//设置返利现金券
+	posts["/setRebateRMB"] = function(req,res) {
+		var data = req.body
+		var info = {
+			"title" : data.title,
+			"text" : data.text,
+			"rate" : data.rate,
+			"beginTime" : data.beginTime,
+			"endTime" : data.endTime
+		}
+		self.redisDao.db.hset("rebate_rmb_map",data.id,JSON.stringify(info),function(err) {
+			var url = "http://127.0.0.1:5081/updateRebate"
+			http.get(url,function(res){})
+			res.send("SUCCESS")
+		})
+	}
+	//删除返利现金券
+	posts["/delRebateRMB"] = function(req,res) {
+		var data = req.body
+		self.redisDao.db.hdel("rebate_rmb_map",data.id,function(err) {
+			var url = "http://127.0.0.1:5081/updateRebate"
+			http.get(url,function(res){})
+			res.send("SUCCESS")
 		})
 	}
 	//设置服务器名称
