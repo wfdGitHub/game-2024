b18b3fb4142725fcd6340fc343b49e4fdc104808
wfd <752387064@qq.com>
2023-01-18 15:06:28 +0800

    更新

--- game-server/app/domian/area/area.js
+++ game-server/app/domian/area/area.js
@@ -110,6 +110,7 @@ area.prototype.dayUpdate = function(curDayStr) {
 	this.activityDayUpdate()
 	this.worldLevelInit()
 	this.worldLevelDayUpdate()
+	this.rebateInit()
 	this.getAreaObj("areaInfo","dayStr",function(data) {
 		if(data !== self.dayStr){
 			self.setAreaObj("areaInfo","dayStr",self.dayStr)
--- game-server/app/domian/area/areaServer/rebate.js
+++ game-server/app/domian/area/areaServer/rebate.js
@@ -1,5 +1,5 @@
 //返利活动
-const oneDayTime = 86399999
+const oneDayTime = 172799998  //两天时间  截止时间为当日0点-次日24点
 module.exports = function() {
 	var self = this
 	this.rebate_item_map = {}
@@ -15,26 +15,67 @@ module.exports = function() {
 	this.rebate_gold = {
 		"title" : "返利标题",
 		"text" : "返利内容",
-		"rate" : 0
+		"rate" : 0,
+		"beginTime" : 0,
+		"endTime" : 0
+	}
+	this.rebate_rmb_map = {}
+	this.rebate_rmb = {
+		"title" : "返利标题",
+		"text" : "返利内容",
+		"rate" : 0,
+		"beginTime" : 0,
+		"endTime" : 0
 	}
 	this.rebateInit = function() {
 		// console.log("rebateInit")
+		var curTime = Date.now()
 		self.redisDao.db.hgetall("rebate_item_map",function(err,data) {
 			if(data){
 				self.rebate_item_map = data
 				for(var i in data){
 					self.rebate_item_map[i] = JSON.parse(data[i])
+					self.rebate_item_map[i]["endTime"] += oneDayTime
+					if(curTime < self.rebate_item_map[i]["beginTime"] || curTime > self.rebate_item_map[i]["endTime"])
+						delete self.rebate_item_map[i]
 				}
 			}
 			else
 				self.rebate_item_map = {}
 			// console.log("rebate_item_map",self.rebate_item_map)
 		})
+		self.redisDao.db.hgetall("rebate_rmb_map",function(err,data) {
+			if(data){
+				self.rebate_rmb_map = data
+				for(var i in data){
+					self.rebate_rmb_map[i] = JSON.parse(data[i])
+					if(!self.rebate_rmb_map[i]["beginTime"])
+						self.rebate_rmb_map[i]["beginTime"] = 0
+					if(!self.rebate_rmb_map[i]["endTime"])
+						self.rebate_rmb_map[i]["endTime"] = 10382256000000
+					self.rebate_rmb_map[i]["endTime"] += oneDayTime
+					self.rebate_rmb_map[i]["rate"] = Number(self.rebate_rmb_map[i]["rate"]) || 0
+					if(curTime < self.rebate_rmb_map[i]["beginTime"] || curTime > self.rebate_rmb_map[i]["endTime"])
+						delete self.rebate_rmb_map[i]
+				}
+			}
+			else
+				self.rebate_rmb_map = {}
+			// console.log("rebate_rmb_map",self.rebate_rmb_map)
+		})
 		self.redisDao.db.hgetall("rebate_gold_map",function(err,data) {
 			if(data){
 				self.rebate_gold_map = data
 				for(var i in data){
 					self.rebate_gold_map[i] = JSON.parse(data[i])
+					if(!self.rebate_gold_map[i]["beginTime"])
+						self.rebate_gold_map[i]["beginTime"] = 0
+					if(!self.rebate_gold_map[i]["endTime"])
+						self.rebate_gold_map[i]["endTime"] = 10382256000000
+					self.rebate_gold_map[i]["endTime"] += oneDayTime
+					self.rebate_gold_map[i]["rate"] = Number(self.rebate_gold_map[i]["rate"]) || 0
+					if(curTime < self.rebate_gold_map[i]["beginTime"] || curTime > self.rebate_gold_map[i]["endTime"])
+						delete self.rebate_gold_map[i]
 				}
 			}
 			else
@@ -51,7 +92,7 @@ module.exports = function() {
 		var curTime = Date.now()
 		if(self.players[uid].userDay > 1){
 			for(var i in self.rebate_item_map){
-				if(real_day >= self.rebate_item_map[i]["rmb"] && curTime >= self.rebate_item_map[i]["beginTime"] && curTime <= (self.rebate_item_map[i]["endTime"] + oneDayTime)){
+				if(real_day >= self.rebate_item_map[i]["rmb"]){
 					self.sendMail(uid,self.rebate_item_map[i]["title"],self.rebate_item_map[i]["text"],self.rebate_item_map[i]["award"])
 				}
 			}
@@ -65,6 +106,17 @@ module.exports = function() {
 			}
 			if(self.rebate_gold_map[index]){
 				self.sendMail(uid,self.rebate_gold_map[index]["title"],self.rebate_gold_map[index]["text"],"200:"+Math.floor(real_day/100 * self.rebate_gold_map[index]["rate"]))
+			}
+			var index = 0
+			for(var i in self.rebate_rmb_map){
+				if(real_day >= i){
+					index = Number(i)
+				}else{
+					break
+				}
+			}
+			if(self.rebate_rmb_map[index]){
+				self.sendMail(uid,self.rebate_rmb_map[index]["title"],self.rebate_rmb_map[index]["text"],"110:"+Math.floor(real_day/100 * self.rebate_rmb_map[index]["rate"]))
 			}
 		}
 	}
