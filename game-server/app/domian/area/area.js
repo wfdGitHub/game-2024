866361b4f7c8a6f59449f0a0c6d22d70a9f6d2f9
wfd <752387064@qq.com>
2023-02-01 15:12:09 +0800

    更新

--- game-server/app/domian/area/area.js
+++ game-server/app/domian/area/area.js
@@ -122,6 +122,7 @@ area.prototype.dayUpdate = function(curDayStr) {
 	this.extremityInit()
 	this.taskDayUpdate()
 	this.updateFestivalInfo()
+	this.activityDayUpdate()
 	this.rebateInit()
 	this.getAreaObj("areaInfo","dayStr",function(data) {
 		if(data !== self.dayStr){
--- web-server/serverDB.js
+++ web-server/serverDB.js
@@ -1,6 +1,7 @@
 //数据库查询
 const http = require("http")
 const uuid = require("uuid")
+const os = require('os');
 const querystring = require("querystring")
 const item_cfg = require("../game-server/config/gameCfg/item.json")
 const pay_cfg = require("../game-server/config/gameCfg/pay_cfg.json")
@@ -12,6 +13,7 @@ const lv_cfg = require("../game-server/config/gameCfg/lv_cfg.json")
 const star_base = require("../game-server/config/gameCfg/star_base.json")
 const stringRandom = require('string-random');
 const dataClean = require("./model/dataClean.js")
+const sqlClean = require("./model/sqlClean.js")
 var model = function() {
 	var self = this
 	var posts = {}
@@ -22,20 +24,42 @@ var model = function() {
 		self.redisDao = redisDao
 		self.server = server
 		dataClean.init(server,mysqlDao,redisDao)
+		sqlClean.init(server,mysqlDao,redisDao)
 		for(var key in posts){
 			server.post(key,posts[key])
 		}
         for(var i in item_cfg){
-			items[i] = item_cfg[i]["name"]
+        	items[i] = item_cfg[i]["name"]+"("+item_cfg[i]["des"]+")"
+			// items[i] = item_cfg[i]["name"]
         }
+        items["item_cfg"] = item_cfg
 	}
 	//获取直充表
 	posts["/getRechargeCfg"] = function(req,res) {
 		res.send(recharge_cfg)
 	}
-	//获取直充表
-	posts["/getRechargeCfg"] = function(req,res) {
-		res.send(recharge_cfg)
+	//获取系统数据
+	posts["/getOSData"] = function(req,res) {
+		var info = {}
+		info.freemem = os.freemem()
+		info.totalmem = os.totalmem()
+		info.cpus = os.cpus()
+		info.uptime = os.uptime()
+		res.send(info)
+	}
+	//获取英雄表
+	posts["/getHeros"] = function(req,res) {
+		res.send(heros)
+	}
+	//数据库清档
+	posts["/sqlClean"] = function(req,res) {
+		sqlClean.dataClean(function(flag,data) {
+			if(flag){
+				res.send("SUCCESS")
+			}else{
+				res.send(data)
+			}
+		})
 	}
 	//获取全服邮件
 	posts["/getAreaMailList"] = function(req,res) {
