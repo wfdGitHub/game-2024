f73bd2fe0b97f4ea2cb0962aedb52e084058e955
wfd <752387064@qq.com>
2023-02-10 17:55:56 +0800

    更新

--- web-server/serverDB.js
+++ web-server/serverDB.js
@@ -13,6 +13,7 @@ const lv_cfg = require("../game-server/config/gameCfg/lv_cfg.json")
 const star_base = require("../game-server/config/gameCfg/star_base.json")
 const stringRandom = require('string-random');
 const dataClean = require("./model/dataClean.js")
+const sqlClean = require("./model/sqlClean.js")
 var model = function() {
 	var self = this
 	var posts = {}
@@ -23,6 +24,7 @@ var model = function() {
 		self.redisDao = redisDao
 		self.server = server
 		dataClean.init(server,mysqlDao,redisDao)
+		sqlClean.init(server,mysqlDao,redisDao)
 		for(var key in posts){
 			server.post(key,posts[key])
 		}
@@ -48,6 +50,16 @@ var model = function() {
 	//获取英雄表
 	posts["/getHeros"] = function(req,res) {
 		res.send(heros)
+	}
+	//数据库清档
+	posts["/sqlClean"] = function(req,res) {
+		sqlClean.dataClean(function(flag,data) {
+			if(flag){
+				res.send("SUCCESS")
+			}else{
+				res.send(data)
+			}
+		})
 	}
 	//获取全服邮件
 	posts["/getAreaMailList"] = function(req,res) {
