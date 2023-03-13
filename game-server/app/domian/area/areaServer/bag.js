334f42db7e2ed532e17fb509c9ac528f2d38e2ca
wfd <752387064@qq.com>
2023-03-13 09:45:22 +0800

    更新

--- game-server/app/domian/area/areaServer/bag.js
+++ game-server/app/domian/area/areaServer/bag.js
@@ -292,13 +292,15 @@ module.exports = function() {
 					self.gainHeroSkin(uid,itemCfg[itemId]["arg"],cb)
 				return {type : "skin",itemId : itemId}
 				case "hero":
-					var star = itemCfg[itemId]["useType"]
-					var heroInfo = self.heroDao.gainHero(self.areaId,uid,{id : itemCfg[itemId]["arg"],star : star},cb)
-					var notify = {
-						"type" : "newHero",
-						"heroInfo" : heroInfo
+					for(var i = 0;i < value;i++){
+						var star = itemCfg[itemId]["useType"]
+						var heroInfo = self.heroDao.gainHero(self.areaId,uid,{id : itemCfg[itemId]["arg"],star : star},cb)
+						var notify = {
+							"type" : "newHero",
+							"heroInfo" : heroInfo
+						}
+						self.sendToUser(uid,notify)
 					}
-					self.sendToUser(uid,notify)
 				return {type : "item",itemId : itemId,value : value}
 				default:
 					self.addItemCB(uid,itemId,value,function(flag,curValue) {
