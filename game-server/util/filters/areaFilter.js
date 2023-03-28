a6a7646ae6b5de86556ed299208bb342d6f46dcb
wfd <752387064@qq.com>
2023-03-28 10:24:49 +0800

    更新

--- game-server/util/filters/areaFilter.js
+++ game-server/util/filters/areaFilter.js
@@ -5,37 +5,11 @@ module.exports = function() {
 }
 var Filter = function() {}
 Filter.prototype.before = function (msg, session, next) {
-		// var last_route = session.get("last_route")
-		// var count_route = session.get("count_route") || 0
-		// if(last_route == msg.__route__){
-		// 	count_route++
-		// }else{
-		// 	last_route = msg.__route__
-		// 	count_route = 0
-		// }
-    // session.set("last_route",last_route)
-    // session.push("last_route")
-    // session.set("count_route",count_route)
-    // session.push("count_route")
-		// session.handlerTime = Date.now()
-		// if(count_route >= MAX_COUNT){
-		// 	if(count_route == MAX_COUNT){
-		// 	  var redisDao = bearcat.getBean("redisDao")
-		// 	  var info = {
-		// 	  	uid : session.uid,
-		// 	  	last_route : last_route,
-		// 	  	count_route : count_route
-		// 	  }
-		// 	  redisDao.db.rpush("server:logs",JSON.stringify(info),function(err,num) {
-		// 	      if(num > 200){
-		// 	        redisDao.db.ltrim("server:logs",-200,-1)
-		// 	      }
-		// 	  })
-		// 	}
-		//   next("error")
-		//   return
-		// }
+	if(!session.uid || !session.get("areaId") || !session.get("oriId")){
+		next("未登录服务器")
+	}else{
 		next();
+	}
 }
 Filter.prototype.after = function (err, msg, session, resp, next) {
 	var dt = Date.now() - session.handlerTime
