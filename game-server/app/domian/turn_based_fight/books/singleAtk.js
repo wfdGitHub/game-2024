fc1ebbfbd3a581f681c971adb4db6728ac241476
wfd <752387064@qq.com>
2023-05-24 10:57:09 +0800

    更新

--- game-server/app/domian/turn_based_fight/books/singleAtk.js
+++ game-server/app/domian/turn_based_fight/books/singleAtk.js
@@ -5,15 +5,15 @@ var model = function(otps) {
 	var book = new bookBasic(otps)
 	book.bookId = "singleAtk"
 	book.action = function() {
-		var target = book.locator.getTargets(book.team[0],"enemy_1")
-		if(target.length){
+		var target = book.locator.getTargets(book.team[0],"enemy_1")[0]
+		if(target){
 			var recordInfo = {type : "book",bookId:book.bookId,belong : book.belong,targets:[]}
 			var power = book.getTotalAtt("atk")
-			power = Math.max(power - target[0].getTotalAtt("phyDef"),power * 0.1)
+			power = Math.max(power - target.getTotalAtt("phyDef"),power * 0.1)
 			var value = Math.ceil(power * this.mul)
 			if(value < 1)
 				value = 1
-			var info = target[0].onHit(book,{value:value,d_type:"phy"})
+			var info = target.onHit(book,{value:value,d_type:"phy"})
 			recordInfo.targets.push(info)
 			fightRecord.push(recordInfo)
 		}
