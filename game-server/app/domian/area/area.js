2a658a7d5ef06f83b949f58183f322bc3accba38
wfd <752387064@qq.com>
2023-06-29 18:52:31 +0800

    更新

--- game-server/app/domian/area/area.js
+++ game-server/app/domian/area/area.js
@@ -364,10 +364,8 @@ area.prototype.getBaseUser = function(uid) {
 }
 //基准战力阵容
 area.prototype.standardTeam = function(uid,list,dl,lv) {
-	console.log("standardTeam111",uid,list,dl,lv)
-	if(!lv)
+	if(lv === undefined)
 		lv = this.getLordLv(uid)
-	console.log("standardTeam222",uid,list,dl,lv)
 	return this.fightContorl.standardTeam(list,dl,lv)
 }
 //获取玩家单项数据
