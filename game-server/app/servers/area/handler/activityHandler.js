d7382283259b8bef4ff4b9d32fc8ef9f7dc9db41
wfd <752387064@qq.com>
2023-07-28 11:46:17 +0800

    更新

--- game-server/app/servers/area/handler/activityHandler.js
+++ game-server/app/servers/area/handler/activityHandler.js
@@ -34,7 +34,8 @@ activityHandler.prototype.apply_recharge = function(msg, session, next) {
 //获取小七gamesign
 activityHandler.prototype.x7syGameSign = function(msg, session, next) {
   var str = msg.str
-  var gameSign = this.sdkPay.x7syGameSign(str)
+  var os = msg.os
+  var gameSign = this.sdkPay.x7syGameSign(str,os)
   next(null,{flag : true,gameSign : gameSign})
 }
 //申请支付测试接口
