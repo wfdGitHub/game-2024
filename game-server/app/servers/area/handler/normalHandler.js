c3f808438eff62bf18179834b4cf298a235b6c88
wfd <752387064@qq.com>
2023-09-06 10:53:56 +0800

    SDK更新

--- game-server/app/servers/area/handler/normalHandler.js
+++ game-server/app/servers/area/handler/normalHandler.js
@@ -1,6 +1,7 @@
 var bearcat = require("bearcat")
 var async = require("async")
 var default_cfg = require("../../../../config/gameCfg/default_cfg.json")
+var sdkConfig = require("../../../../config/gameCfg/sdkConfig.json")
 var normalHandler = function(app) {
   this.app = app;
 	this.areaManager = this.app.get("areaManager")
@@ -195,12 +196,27 @@ normalHandler.prototype.changeName = function(msg, session, next) {
   var areaId = session.get("areaId")
   var oriId = session.get("oriId")
   var name = msg.name
+  var guid = msg.guid
+  var os = msg.os
   var self = this
   if(name.indexOf(".") != -1){
     next(null,{flag:false,err:"不能包含特殊字符"})
     return
   }
   async.waterfall([
+    function(cb) {
+      //小七敏感词检测
+      if(sdkConfig.sdk_type["value"] == "x7sy"){
+        self.sdkQuery.x7syMessageDetect(guid,os,name,function(flag,message,level) {
+          if(!flag || level != 1)
+            cb("名称含有敏感信息")
+          else
+            cb()
+        })
+      }else{
+        cb()
+      }
+    },
     function(cb) {
       self.redisDao.db.hexists("game:nameMap",name,function(err,data) {
         if(data){
@@ -247,6 +263,9 @@ normalHandler.prototype.initNameAndSex = function(msg, session, next) {
   var oriId = session.get("oriId")
   var name = msg.name
   var sex = msg.sex
+  var guid = msg.guid
+  var os = msg.os
+  var type = msg.type
   var self = this
   if(!name || (sex !== 1 && sex !== 2)){
     next(null,{flag:false,err:"参数错误"})
@@ -255,6 +274,19 @@ normalHandler.prototype.initNameAndSex = function(msg, session, next) {
   if(sex !== 1)
     sex = 2
   async.waterfall([
+    function(cb) {
+      //小七敏感词检测
+      if(!type && sdkConfig.sdk_type["value"] == "x7sy"){
+        self.sdkQuery.x7syMessageDetect(guid,os,name,function(flag,message,level) {
+          if(!flag || level != 1)
+            cb("名称含有敏感信息")
+          else
+            cb()
+        })
+      }else{
+        cb()
+      }
+    },
     function(cb) {
       self.redisDao.db.hexists("game:nameMap",name,function(err,data) {
         if(data){
@@ -397,6 +429,9 @@ module.exports = function(app) {
     },{
       name : "CDKeyDao",
       ref : "CDKeyDao"
+    },{
+      name : "sdkQuery",
+      ref : "sdkQuery"
     }]
   })
 };
