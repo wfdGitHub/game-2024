23c15de5eb3318ae0ef4829a5d2218e8043e50dd
wfd <752387064@qq.com>
2023-09-20 17:59:03 +0800

    Revert "SDK读配置表"
    This reverts commit 9818fcf8c4dbefe937dca8cffbe7fac48b2b0a85.

--- game-server/app/domian/sdk/sdkPay.js
+++ game-server/app/domian/sdk/sdkPay.js
@@ -8,24 +8,21 @@ var local = {}
 //初始化获取配置
 model.prototype.init = function(cb) {
 	// console.log("SKD支付模块 初始化SDK配置")
-	delete require.cache[require.resolve('../../../config/gameCfg/sdkConfig.json')]; 
-	this.sdkConfig = require("../../../config/gameCfg/sdkConfig.json")
-	for(var i in this.sdkConfig)
-		if(Number.isFinite(this.sdkConfig[i]["value"]))
-			this.sdkConfig[i]["value"] = this.sdkConfig[i]["value"].toFixed()
+	delete require.cache[require.resolve('../../../config/sysCfg/sdkConfig.json')]; 
+	this.sdkConfig = require("../../../config/sysCfg/sdkConfig.json")
 }
 //收到支付回调
 model.prototype.pay_order = function(data,finish_callback,req,res) {
-	switch(this.sdkConfig.sdk_type["value"]){
+	switch(this.sdkConfig.sdk_type){
 		case "quick":
 			this.quick_order(data,finish_callback,req,res)
 		break
-		// case "jianwan":
-		// 	this.jianwan_order(data,finish_callback,req,res)
-		// break
-		// case "277":
-		// 	this.game277_order(data,finish_callback,req,res)
-		// break
+		case "jianwan":
+			this.jianwan_order(data,finish_callback,req,res)
+		break
+		case "277":
+			this.game277_order(data,finish_callback,req,res)
+		break
 		case "x7sy":
 			this.x7sy_order(data,finish_callback,req,res)
 		break
@@ -40,7 +37,7 @@ model.prototype.quick_order = function(data,finish_callback,req,res) {
 	}
 	res.send("SUCCESS")
 	var self = this
-	var xmlStr = local.decode(data.nt_data,this.sdkConfig["Callback_Key"]["value"])
+	var xmlStr = local.decode(data.nt_data,this.sdkConfig["Callback_Key"])
 	parseString(xmlStr,function(err,result) {
 		var message = result.quicksdk_message.message[0]
 		var info = {
@@ -140,12 +137,7 @@ model.prototype.game277_order = function(data,finish_callback,req,res) {
 }
 //小七手游订单
 model.prototype.x7sy_order = function(data,finish_callback,req,res) {
-	var publicKey = ""
-	if(data.extends_info_data == 1){
-		publicKey = "-----BEGIN PUBLIC KEY-----\n"+this.sdkConfig["iosAppKey"]["value"]+"\n-----END PUBLIC KEY-----"
-	}else{
-		publicKey = "-----BEGIN PUBLIC KEY-----\n"+this.sdkConfig["RSA"]["value"]+"\n-----END PUBLIC KEY-----"
-	}
+	var publicKey = "-----BEGIN PUBLIC KEY-----\n"+this.sdkConfig["RSA"]+"\n-----END PUBLIC KEY-----"
 	var raw_sign_data = Buffer.from(data.sign_data, 'base64')
 	delete data.sign_data
 	var source_str = local.ksort(data)
@@ -182,11 +174,8 @@ model.prototype.x7sy_order = function(data,finish_callback,req,res) {
 	})
 }
 //小七订单sign
-model.prototype.x7syGameSign = function(str,os) {
-	if(os == "ios")
-		str += this.sdkConfig["iosRSA"]["value"]
-	else
-		str += this.sdkConfig["RSA"]["value"]
+model.prototype.x7syGameSign = function(str) {
+	str += this.sdkConfig["RSA"]
 	var md5 = util.md5(str)
 	return md5
 }
