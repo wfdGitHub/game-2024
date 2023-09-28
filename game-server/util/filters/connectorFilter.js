5a593280b636362a2cf1b799452e1327061981a6
wfd <752387064@qq.com>
2023-09-28 10:22:40 +0800

    更新

--- game-server/util/filters/connectorFilter.js
+++ game-server/util/filters/connectorFilter.js
@@ -3,14 +3,20 @@ const MAX_COUNT = 50
 module.exports = function() {
   return new Filter();
 }
-var Filter = function() {}
+var Filter = function() {
+	this.redisDao = bearcat.getBean("redisDao")
+}
 Filter.prototype.before = function (msg, session, next) {
+	session.handlerTime = Date.now()
 		next();
 }
 Filter.prototype.after = function (err, msg, session, resp, next) {
 	var dt = Date.now() - session.handlerTime
-	if(dt > 1000){
-		console.error("handlerTime over ",dt,msg)
+	this.redisDao.db.zincrby("logs:callPort:count",1,msg.__route__)
+	this.redisDao.db.zincrby("logs:callPort:dt",dt,msg.__route__)
+	if(session.uid){
+		this.redisDao.db.zincrby("logs:callUid:count",1,session.uid)
+		this.redisDao.db.zincrby("logs:callUid:dt",dt,session.uid)
 	}
 	next(err);
 }
