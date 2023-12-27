bd57ee1862d3e28581bb7ea2d4d5269feb15c86a
wfd <752387064@qq.com>
2023-12-27 16:40:50 +0800

    更新

--- game-server/app/dao/payDao.js
+++ game-server/app/dao/payDao.js
@@ -33,7 +33,7 @@ payDao.prototype.createGameOrder = function(otps,cb) {
 					if(err || !data){
 						next("未定制英雄")
 					}else{
-						info.amount = Number(data)
+						info.amount = Math.ceil(Number(data) / 100)
 						next()
 					}
 				})
