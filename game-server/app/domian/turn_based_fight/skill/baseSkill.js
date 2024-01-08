45491e33e6d748a62addb54b42f9c7730a6525d8
wfd <752387064@qq.com>
2024-01-08 18:00:16 +0800

    更新

--- game-server/app/domian/turn_based_fight/skill/baseSkill.js
+++ game-server/app/domian/turn_based_fight/skill/baseSkill.js
@@ -10,6 +10,7 @@ var model = function(otps,character) {
 	this.targetType = otps.targetType || "enemy_normal"	//目标类型  normal
 	this.mul = otps.mul || 0							//技能系数
 	this.needAnger = otps.anger || 4 					//技能所需怒气
+	this.diedSkill = otps.diedSkill  					//死亡释放技能
 	this.skill_buffs = {}								//技能附带buff
 	//初始化参数
 	this.initArg = function() {}
