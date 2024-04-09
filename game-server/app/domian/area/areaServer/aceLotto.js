db36cf0b4f593dc7c945c27721c1796932e493ce
wfd <752387064@qq.com>
2024-04-09 16:54:22 +0800

    更新

--- game-server/app/domian/area/areaServer/aceLotto.js
+++ game-server/app/domian/area/areaServer/aceLotto.js
@@ -6,7 +6,7 @@ for(var i in ace_pack){
 		aceList[ace_pack[i]["quality"]] = []
 	aceList[ace_pack[i]["quality"]].push(i)
 }
-const weight = {"3":0.35,"4":0.24,"5":0.12,"6":0.08,"7":0}
+const weight = {"3":0.35,"4":0.24,"5":0.14,"6":0,"7":0}
 module.exports = function() {
 	var self = this
 	this.compoundAce = function(uid,aId1,aId2,cb) {
